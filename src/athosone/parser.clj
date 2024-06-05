035e814a51cd0bd4cf4c17e52171aac5ad94f3b2
Athosone <werckayrton@gmail.com>
2024-06-05 23:59:05 +0200

    WIP - 6.2.1 -> TODO parenthesis grouping

--- src/athosone/parser.clj
+++ src/athosone/parser.clj
@@ -44,68 +44,77 @@
         advanced (advance parser)]
     (if (or (= operator-type ::token/minus)
             (= operator-type ::token/banq))
-      (let [right (unary advanced)]
-        (ast/unary operator right))
+      (let [right (unary advanced)
+            expr (:expr right)]
+        (append-expr right (ast/unary operator expr)))
       (primary parser))))
 
 (defn factor [parser]
-  (loop [p (unary parser)
-         e (:expr p)]
+  (loop [p (unary parser)]
     (let [operator (current p)
-          operator-type (::token/type operator)]
+          operator-type (::token/type operator)
+          advanced (advance p)
+          lhs (:expr p)]
       (if (or (= operator-type ::token/star)
               (= operator-type ::token/slash))
-        (recur (advance p)
-               (append-expr p (ast/binary e operator (unary (advance p)))))
-        (append-expr p e)))))
+        (recur (append-expr (unary advanced) (ast/binary lhs operator (:expr (unary advanced)))))
+        p))))
 
 (defn term [parser]
-  (loop [p (factor parser)
-         e (:expr p)]
+  (loop [p (factor parser)]
     (let [operator (current p)
-          operator-type (::token/type operator)]
+          operator-type (::token/type operator)
+          advanced (advance p)
+          lhs (:expr p)]
       (if (or (= operator-type ::token/minus)
               (= operator-type ::token/plus))
-        (recur (advance p)
-               (append-expr p (ast/binary e operator (factor (advance p)))))
-        (append-expr p e)))))
+        (recur (append-expr (factor advanced) (ast/binary lhs operator (:expr (factor advanced)))))
+        p))))
 
 (defn comparison [parser]
-  (loop [p (term parser)
-         e (:expr p)]
+  (loop [p (term parser)]
     (let [operator (current p)
-          operator-type (::token/type operator)]
+          operator-type (::token/type operator)
+          advanced (advance p)
+          lhs (:expr p)]
       (if (or (= operator-type ::token/greater-equal)
               (= operator-type ::token/greater)
               (= operator-type ::token/less-equal)
               (= operator-type ::token/less))
-        (recur (advance p)
-               (append-expr p (ast/binary e operator (term (advance p)))))
-        (append-expr p e)))))
+        (recur (append-expr (term advanced) (ast/binary lhs operator (:expr (term advanced)))))
+        p))))
 
 (defn equality [parser]
-  (loop [p (comparison parser)
-         e (:expr p)]
+  (loop [p (comparison parser)]
     (let [operator (current p)
-          operator-type (::token/type operator)]
-      (println "operator-type" operator-type)
+          operator-type (::token/type operator)
+          advanced (advance p)
+          lhs (:expr p)]
       (if (or (= operator-type ::token/equal-equal) (= operator-type ::token/bang-equal))
-        (recur (advance p) (append-expr p (ast/binary e operator (comparison (advance p)))))
-        (append-expr p e)))))
+        (let [right (comparison advanced)
+              right-expr (:expr right)]
+          (recur (append-expr right (ast/binary lhs operator right-expr))))
+        p))))
 
 (defn expression [parser] (equality parser))
 
 (comment
-  (require '[athosone.scanner.scan :refer [scan-tokens new-scanner]])
-  (def source "-1 == 1")
+  (require
+   '[athosone.prettyprinter :refer [pretty-print]]
+   '[athosone.scanner.scan :refer [scan-tokens new-scanner]])
+  (def source "1 == 1")
   (def tokens (scan-tokens (new-scanner source)))
   (def p (new-parser tokens))
 
+  (pretty-print (:expr (expression (new-parser (scan-tokens (new-scanner "1 * -1 - -2 == 3"))))))
   (prn p)
   (assoc-in p [:expr] "hellp")
 
   (:current (expression p))
   (:expr (unary p))
-  (primary p)
+  (primary (new-parser (scan-tokens (new-scanner "1 + 1"))))
+  (:expr (unary (new-parser (scan-tokens (new-scanner "-1 * 1")))))
+  (:expr (factor (new-parser (scan-tokens (new-scanner "-1 * -1 / 0")))))
+  (factor (new-parser (scan-tokens (new-scanner "-1 * 1 / 0"))))
   (:expr (equality p))
   ())
